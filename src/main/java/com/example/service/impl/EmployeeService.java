0cc3e4e0a3023e82716391cdb0a80b1a0a4c052b
cmit <282259956@qq.com>
2022-08-08 21:40:17 +0800

    1.登录功能：   1.1搭建框架

---
+++ src/main/java/com/example/service/impl/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.example.service.impl;
+
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.entity.Employee;
+
+public interface EmployeeService extends IService<Employee> {
+}
