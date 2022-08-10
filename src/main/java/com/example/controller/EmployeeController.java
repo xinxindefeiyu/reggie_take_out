1cc24ed393cfa3132686ff2381bd8c68f7d6d823
cmit <282259956@qq.com>
2022-08-10 09:03:00 +0800

    1.登录功能：   1.1搭建框架   1.2登录   1.3退出   1.4过滤器

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -1,10 +1,15 @@
 package com.example.controller;
 
-import com.example.service.impl.EmployeeService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.example.common.R;
+import com.example.entity.Employee;
+import com.example.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @RestController
@@ -13,6 +18,52 @@ public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
+
+    /**
+     * 员工登录
+     * @param request
+     * @param employee
+     * @return
+     */
+    @PostMapping("/login")
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee) {
+
+        //1、将页面提交的密码password进行md5加密处理
+        String password = employee.getPassword();
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+
+        //2、根据页面提交的用户名username查询数据库
+        LambdaQueryWrapper<Employee> query = new LambdaQueryWrapper<>();
+        query.eq(Employee::getUsername, employee.getUsername());
+        Employee emp = employeeService.getOne(query);
+
+        //3、如果没有查询到则返回登录失败结果
+        if (emp == null) {
+            return R.error("没有此用户");
+        }
+
+        //4、密码比对，如果不一致则返回登录失败结果
+        if (!emp.getPassword().equals(password)) {
+            return R.error("密码错误");
+        }
+
+        //5、查看员工状态，如果为已禁用状态，则返回员工已禁用结果
+        if (emp.getStatus() == 0) {
+            return R.error("账号已禁用");
+        }
+
+        //6、登录成功，将员工id存入Session并返回登录成功结果
+        request.getSession().setAttribute("employee", emp.getId());
+        return R.success(emp);
+
+    }
+
+    @PostMapping("/logout")
+    public R<String> logout(HttpServletRequest request) {
+        request.getSession().removeAttribute("employee");
+        return R.success("退出成功");
+    }
+
 
 
 }
--- src/main/java/com/example/service/impl/EmployeeService.java
+++
@@ -1,8 +0,0 @@
-package com.example.service.impl;
-
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.entity.Employee;
-
-public interface EmployeeService extends IService<Employee> {
-}
