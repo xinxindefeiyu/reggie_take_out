e682f6c276164026fb2fea1c8a3828534b5801cd
cmit <282259956@qq.com>
2022-08-15 22:25:51 +0800

    1.登录功能：   1.1搭建框架   1.2登录   1.3退出   1.4过滤器  2.员工管理功能：    2.1新增员工    2.2全局异常处理    2.3员工列表展示    2.4员工状态修改    2.5员工信息编辑

--- src/main/java/com/example/config/WebMvcConfig.java
+++ src/main/java/com/example/config/WebMvcConfig.java
@@ -1,9 +1,14 @@
 package com.example.config;
 
+import com.example.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -19,5 +24,17 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
 
+    }
+
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        log.info("消息转换器开始加载...");
+
+        //首先创建消息转化器对象
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        //设置消息转化器的底层为JacksonObjectMapper
+        converter.setObjectMapper(new JacksonObjectMapper());
+        //将设置好的消息转化器加入到消息转化器的列表中
+        converters.add(0, converter);
     }
 }
--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -1,10 +1,12 @@
 package com.example.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.common.R;
 import com.example.entity.Employee;
 import com.example.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
@@ -70,6 +72,12 @@ public class EmployeeController {
         return R.success("退出成功");
     }
 
+    /**
+     * 新增员工
+     * @param request
+     * @param employee
+     * @return
+     */
     @PostMapping
     public R<String> save(HttpServletRequest request, @RequestBody Employee employee) {
         log.info("新增员工， 员工信息：{}", employee.toString());
@@ -85,6 +93,71 @@ public class EmployeeController {
         employeeService.save(employee);
         return R.success("新增员工成功");
     }
+
+    /**
+     * 展示员工列表
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name) {
+        log.info("page = {}, pageSize = {}, name = {}", page, pageSize, name);
+
+        //构造分页构造器
+        Page pageInfo = new Page(page, pageSize);
+
+        //构建条件构造器
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+
+        //添加过滤条件
+        queryWrapper.like(StringUtils.isNotEmpty(name), Employee::getName, name);
+
+        //添加排序条件
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+
+        //执行语句
+        employeeService.page(pageInfo, queryWrapper);
+
+        return R.success(pageInfo);
+    }
+
+
+    @PutMapping
+    public  R<String> update(HttpServletRequest request, @RequestBody Employee employee) {
+        log.info("修改员工状态， 员工信息：{}", employee.toString());
+
+        employee.setUpdateTime(LocalDateTime.now());
+        Long empId = (Long) request.getSession().getAttribute("employee");
+        employee.setUpdateUser(empId);
+
+        employeeService.updateById(employee);
+        return R.success("修改员工成功");
+    }
+
+    @GetMapping("/{id}")
+    public R<Employee> getById(@PathVariable String id) {
+        log.info("根据id查找用户，用户Id = {}", id);
+
+        //构建条件构造器
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+
+        //添加过滤条件
+        queryWrapper.eq(Employee::getId, id);
+
+        //执行语句
+        Employee employee = employeeService.getOne(queryWrapper);
+
+        if (employee != null) {
+            return R.success(employee);
+        } else {
+            return R.error("没有查询到该员工");
+        }
+
+
+    }
+
 
 
 
