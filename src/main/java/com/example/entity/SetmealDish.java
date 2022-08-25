ce8bc85dc3171eded3b167f2142b6dbc1c28fdd9
maweixin <maweixin@chinamobile.com>
2022-08-25 21:05:03 +0800

    4.菜品管理   4.1文件上传下载   4.2菜品新增   4.3菜品展示   4.4修改菜品 5.套餐管理   5.1新增套餐   5.2套餐展示   5.3套餐删除 6.移动端开发   6.1 验证码登录   6.2 地址簿管理ing   6.3 菜品展示   6.4 购物车   6.5 用户下单

---
+++ src/main/java/com/example/entity/SetmealDish.java
@@ -0,0 +1,30 @@
+package com.example.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class SetmealDish {
+
+  private Long id;
+  private Long setmealId;
+  private Long dishId;
+  private String name;
+  private BigDecimal price;
+  private Integer copies;
+  private Integer sort;
+  @TableField(fill = FieldFill.INSERT)
+  private LocalDateTime createTime;
+  @TableField(fill = FieldFill.INSERT_UPDATE)
+  private LocalDateTime updateTime;
+  @TableField(fill = FieldFill.INSERT)
+  private Long createUser;
+  @TableField(fill = FieldFill.INSERT_UPDATE)
+  private Long updateUser;
+  private Integer isDeleted;
+
+}
